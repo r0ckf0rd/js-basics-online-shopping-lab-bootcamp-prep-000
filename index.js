a56f13085bc67d9d4c98ef04c7b617e2a975dd00
Nicholas Werner <micropacman@gmail.com>
2018-09-16 20:56:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
-      cart.splice(i-1, 1)
+      cart.splice(i, 1)
     }
   }
 }
