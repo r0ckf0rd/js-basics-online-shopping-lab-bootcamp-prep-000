ba5e2ba9461b5876eee8b32a548903d37784304a
Nicholas Werner <micropacman@gmail.com>
2018-09-16 19:58:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var cartCount = cart.length
+var cartCount = cart.length
  cart[cartCount] = {itemName: `${item}`, itemPrice: Math.floor(Math.random()*100) }
  return `${item} has been added to your cart.`
 }
