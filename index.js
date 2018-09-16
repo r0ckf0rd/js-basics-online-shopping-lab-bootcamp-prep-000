64df21879679ad15297108da004e5b6181c1dfc8
Nicholas Werner <micropacman@gmail.com>
2018-09-16 20:17:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,9 +30,10 @@ function viewCart() {
   for (var i = 0; i < cart.length; i++) {
     if (i === cart.length - 1) {
       inYourCart = `${inYourCart}, and ${cart[i].itemName} at $${cart[i].itemPrice}.`
-      return inYourCart
+      
     }
   }
+  return inYourCart
   }
 }
 
