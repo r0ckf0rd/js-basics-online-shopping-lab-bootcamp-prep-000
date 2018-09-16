1ac1bed3a0107a5186cc57d20a00619096b6d69e
Nicholas Werner <micropacman@gmail.com>
2018-09-16 21:07:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else {
-    var receipt = `Your total cost is ${totalValue}
+    var receipt = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    cart = []
   }
 }
