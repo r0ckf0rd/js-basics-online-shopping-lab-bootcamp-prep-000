9f709664eca3c793db2166e6488c271d149c62aa
Nicholas Werner <micropacman@gmail.com>
2018-09-16 20:29:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,9 +26,17 @@ function viewCart() {
     return `${inYourCart} ${cart[0].itemName} at $${cart[0].itemPrice}.`
   }
   if (cart.length > 1) {
-  for (var i = 0; i < cart.length; i++) {
-    if (i === cart.length - 1) {
-      inYourCart = `${inYourCart}, and ${cart[i].itemName} at $${cart[i].itemPrice}.`
+    for (var i = 0; i < cart.length; i++) {
+      if (i === (cart.length - 1)) {
+        inYourCart = `${inYourCart}, and ${cart[i].itemName} at $${cart[i].itemPrice}.`
+      }
+      else {
+        inYourCart = `${inYourCart} ${cart[i].itemName} at $${cart[i].itemPrice}, `
+      }
+      
+      return inYourCart
+    }
+ 
       
     }
   }
